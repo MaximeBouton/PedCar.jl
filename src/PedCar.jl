ab7d9dbc10a281e49c8c2069099ca33e29ebcffe
MaximeBouton <maxim.bouton@gmail.com>
2018-08-20 16:14:46 -0700

    comment belief updaters

--- src/PedCar.jl
+++ src/PedCar.jl
@@ -16,7 +16,7 @@ using ParticleFilters
 using AutomotiveDrivingModels
 using AutoViz
 using AutomotivePOMDPs
-using AutomotiveSensors
+#using AutomotiveSensors
 using Parameters
 
 export
@@ -51,14 +51,14 @@ include("helpers.jl")
 include("reward.jl")
 include("render_helpers.jl")
 
-export
-    init_car_belief,
-    init_ped_belief,
-    ParticleOverlay,
-    CarState,
-    CarStateEstimator,
-    PedStateEstimator
+#export
+#    init_car_belief,
+#    init_ped_belief,
+#    ParticleOverlay,
+#    CarState,
+#    CarStateEstimator,
+#    PedStateEstimator
 
-include("belief_updater.jl")
+#include("belief_updater.jl")
 
 end # module
